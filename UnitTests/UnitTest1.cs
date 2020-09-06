cf4dbcea4e6e882e6ee8eebccbb6285d5c0e4144
seriouschess <rehayemb@gmail.com>
2020-09-06 14:12:01 -0500

    added some automated tests

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -11,10 +11,7 @@ namespace UnitTests
         private RunwaySelector runwayselector_module = new RunwaySelector();
 
         [SetUp]
-        public void Setup()
-        {
-            //TestClass twelve_module = new TestClass();
-        }
+        public void Setup(){ }
 
         [Test]
         public void ReturnsTwelve_MustBeTwelve()
@@ -30,6 +27,8 @@ namespace UnitTests
             Assert.IsTrue(result == value, $"Method did not return 12");
         }
 
+        //Runway Selector tests
+
         [Test]
         public void SelectRunway_MustBeExpectedValue_A(){
             List<int> runway_d_list = new List<int>();
@@ -43,9 +42,9 @@ namespace UnitTests
         [Test]
         public void SelectRunway_MustBeExpectedValue_B(){
             List<int> runway_d_list = new List<int>();
-            runway_d_list.Add(10);
             runway_d_list.Add(330);
-            runway_d_list.Add(130);
+            runway_d_list.Add(45);
+            runway_d_list.Add(170);
             var result = runwayselector_module.SelectRunway( runway_d_list, 0);
             Assert.IsTrue(result == 2, $"RunwaySelector returned index value {result}");
         }
@@ -58,6 +57,41 @@ namespace UnitTests
             runway_d_list.Add(195);
             var result = runwayselector_module.SelectRunway( runway_d_list, 285);
             Assert.IsTrue(result == 0, $"RunwaySelector returned index value {result}");
+        }
+
+
+        //Optimal Runway Direction Tests
+        [Test]
+        public void SelectOptimalRunwayDirection_MustBeExpectedValue_A(){
+            List<int> runway_d_list = new List<int>();
+            runway_d_list.Add(360);
+            runway_d_list.Add(185);
+            runway_d_list.Add(135);
+            var result = runwayselector_module.SelectOptimalRunwayDirection( runway_d_list, 315);
+            int expected_value = 135;
+            Assert.IsTrue(result == expected_value, $"SelectOptimalRunwayDirection returned angle {result}, Expected value was {expected_value}");
+        }
+
+        [Test]
+        public void SelectOptimalRunwayDirection_MustBeExpectedValue_B(){
+            List<int> runway_d_list = new List<int>();
+            runway_d_list.Add(45);
+            runway_d_list.Add(90);
+            runway_d_list.Add(85);
+            var result = runwayselector_module.SelectOptimalRunwayDirection( runway_d_list, 45);
+            int expected_value = 225;
+            Assert.IsTrue(result == expected_value, $"RunwaySelector returned index value {result}, Expected value was {expected_value}");
+        }
+
+        [Test]
+        public void SelectOptimalRunwayDirection_MustBeExpectedValue_C(){
+            List<int> runway_d_list = new List<int>();
+            runway_d_list.Add( 100 );
+            runway_d_list.Add( 200 );
+            runway_d_list.Add( 300 );
+            var result = runwayselector_module.SelectOptimalRunwayDirection( runway_d_list, 285);
+            int expected_value = 100;
+            Assert.IsTrue(result == expected_value, $"RunwaySelector returned index value {result}, Expected value was {expected_value}");
         }
     }
 }
